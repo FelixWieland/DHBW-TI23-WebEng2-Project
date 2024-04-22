8efc65f946f2072a6592ea9aca7bf38128dd6610
Felix Wieland <felix.wieland@mail.de>
2024-04-22 14:58:48 +0200

    changed position marker

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import {LatLng, LatLngExpression, LeafletMouseEvent, map, Map, marker, tileLayer} from 'leaflet'
+import {divIcon, LatLng, LatLngExpression, LeafletMouseEvent, map, Map, marker, tileLayer} from 'leaflet'
 
 import '../css/interactive-map.css'
 import 'leaflet/dist/leaflet.css'
 import { TileVariant, changeMapLayer } from "../functions/interactiveMap";
+import { Icon } from "framework7-react";
 
 interface MapProps {
   latLngExpression: LatLngExpression
@@ -55,7 +56,11 @@ export const InteractiveMap: React.FC<MapProps> = ({
 
   useEffect(() => {
     if (leaflet && selectedLocation) {
-      const m = marker(selectedLocation).addTo(leaflet);
+      const m = marker(selectedLocation, {
+        icon: divIcon({
+          html: `<svg class="interactive-map-marker-icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PlaceIcon"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7m0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5"></path></svg>`
+        })
+      }).addTo(leaflet);
       return () => { 
         m.removeFrom(leaflet) 
       }
